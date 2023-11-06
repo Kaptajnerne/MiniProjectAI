b03162629a1732bc6f4a7463c9b5a51af3ce8b7b
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-11-06 10:03:50 +0100

    Initial commit

---
+++ src/main/java/com/example/mini_project_ai/MiniProjectAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mini_project_ai/MiniProjectAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
